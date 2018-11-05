9d081955ba3d6b85cd3c29a6efbc6a70c620f791
Kipkemei Isaac <deisaack@gmail.com>
2018-11-05 23:27:51 +0300

    add space

--- mylibrary/src/main/java/com/addictaf/mylibrary/LogDebug.java
+++ mylibrary/src/main/java/com/addictaf/mylibrary/LogDebug.java
@@ -3,6 +3,7 @@ package com.addictaf.mylibrary;
 import android.util.Log;
 
 public class LogDebug {
+
     private static final String TAG = "SUPER_AWESOME_APP";
     public static void d(String message) {
         Log.d(TAG, message);
